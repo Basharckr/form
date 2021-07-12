f950dd3bd60dc38c51a58676242ac25e4bd0dbdd
basharckr <basharpmr@gmail.com>
2021-07-12 15:04:43 +0530

    debug status changed

--- form/settings.py
+++ form/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$c)$=+eeiz*^4v&r@!1&e2%yl%n_y9qa*vlnfws5t#%j65!&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://machinetestform.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
