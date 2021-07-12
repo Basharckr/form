40981a0bf84a8f3df8fc16abd6377cf96f451ece
basharckr <basharpmr@gmail.com>
2021-07-12 16:20:12 +0530

    added libraries config in temples settings.py

--- form/settings.py
+++ form/settings.py
@@ -70,6 +70,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'libraries': {  
+                'staticfiles': 'django.templatetags.static',
+                },
     },
 ]
 
